cffdc98ffe10059baf38693a18dd124379f0fd1c
kauatwn <kaualopescontato@gmail.com>
2024-07-17 15:36:39 -0300

    Ajuste na cor do Notification

--- src/components/Notification/NotificationContent.tsx
+++ src/components/Notification/NotificationContent.tsx
@@ -5,10 +5,10 @@ interface NotificationContentProps {
 export function NotificationContent({ text }: NotificationContentProps) {
   return (
     <div className="flex-1 flex flex-col gap-2">
-      <p className="text-sm leading-relaxed text-zinc-600 dark:text-zinc-100">
+      <p className="text-sm leading-relaxed text-zinc-900 dark:text-zinc-50">
         {text}
       </p>
-      <div className="text-xs text-zinc-400">
+      <div className="text-xs text-zinc-600 dark:text-zinc-400">
         <span>3min ago</span>
       </div>
     </div>
--- src/components/Notification/NotificationRoot.tsx
+++ src/components/Notification/NotificationRoot.tsx
@@ -6,7 +6,7 @@ interface NotificationRootProps {
 
 export function NotificationRoot({ children }: NotificationRootProps) {
   return (
-    <article className="bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
+    <article className="bg-white dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
       {children}
     </article>
   );
