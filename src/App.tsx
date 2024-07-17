1f02fc53ced73912b89084e1dacf7044c4e7ab97
kauatwn <kaualopescontato@gmail.com>
2024-07-17 00:21:15 -0300

    Incluindo componentes no app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,76 @@
+import { Check, Binary, X } from "lucide-react";
+import { Notification } from "./components/Notification";
+import { Widget } from "./components/Widget";
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <>
+      <Widget.Root>
+        <Widget.Header title="Notifications" label="Mark all as read" />
+        <Widget.Content title="Recent">
+          {/* Recent notifications section */}
+          <Notification.Root>
+            <Notification.Icon icon={Binary} />
+            <Notification.Content text="You received an invitation" />
+            <Notification.Actions>
+              <Notification.Action icon={X} />
+              <Notification.Action
+                icon={Check}
+                className="bg-green-500 hover:bg-green-600 dark:bg-green-500 dark:hover:bg-green-600"
+              />
+            </Notification.Actions>
+          </Notification.Root>
+          <Notification.Root>
+            <Notification.Icon icon={Binary} />
+            <Notification.Content text="You received an invitation" />
+            <Notification.Actions>
+              <Notification.Action icon={X} />
+              <Notification.Action
+                icon={Check}
+                className="bg-green-500 hover:bg-green-600 dark:bg-green-500 dark:hover:bg-green-600"
+              />
+            </Notification.Actions>
+          </Notification.Root>
+        </Widget.Content>
+        <Widget.Content title="Older">
+          {/* Older notifications section */}
+          <Notification.Root>
+            <Notification.Icon icon={Binary} />
+            <Notification.Content text="You received an invitation" />
+            <Notification.Actions>
+              <Notification.Action icon={X} />
+              <Notification.Action
+                icon={Check}
+                className="bg-green-500 hover:bg-green-600 dark:bg-green-500 dark:hover:bg-green-600"
+              />
+            </Notification.Actions>
+          </Notification.Root>
+          <Notification.Root>
+            <Notification.Icon icon={Binary} />
+            <Notification.Content text="You received an invitation" />
+            <Notification.Actions>
+              <Notification.Action icon={X} />
+              <Notification.Action
+                icon={Check}
+                className="bg-green-500 hover:bg-green-600 dark:bg-green-500 dark:hover:bg-green-600"
+              />
+            </Notification.Actions>
+          </Notification.Root>
+          <Notification.Root>
+            <Notification.Icon icon={Binary} />
+            <Notification.Content text="You received an invitation" />
+            <Notification.Actions>
+              <Notification.Action icon={X} />
+              <Notification.Action
+                icon={Check}
+                className="bg-green-500 hover:bg-green-600 dark:bg-green-500 dark:hover:bg-green-600"
+              />
+            </Notification.Actions>
+          </Notification.Root>
+        </Widget.Content>
+      </Widget.Root>
+    </>
+  );
 }
 
 export default App;
