66cbef67cf0efe86cf1c34ad6bf0b62dd6808ff4
kauatwn <kaualopescontato@gmail.com>
2024-07-17 15:36:10 -0300

    Ajuste na cor do Widget

--- src/components/Widget/WidgetHeader.tsx
+++ src/components/Widget/WidgetHeader.tsx
@@ -5,9 +5,9 @@ interface WidgetHeaderProps {
 
 export function WidgetHeader({ title, label }: WidgetHeaderProps) {
   return (
-    <header className="bg-zinc-200 dark:bg-zinc-800 py-4 px-6 flex items-center justify-between">
-      <span className="font-bold text-white">{title}</span>
-      <button className="text-green-500 font-bold text-xs hover:text-green-600 uppercase">
+    <header className="bg-zinc-600 dark:bg-zinc-800 py-4 px-6 flex items-center justify-between">
+      <span className="font-bold text-white dark:text-white">{title}</span>
+      <button className="text-green-300 font-bold text-xs hover:text-green-400 uppercase">
         {label}
       </button>
     </header>
