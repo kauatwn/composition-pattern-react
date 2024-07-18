0fc765a7f9ca6c60e423031077e2714a7d25cc68
kauatwn <kaualopescontato@gmail.com>
2024-07-18 18:09:09 -0300

    Ajustando estilos e layout

--- src/components/Widget/WidgetContent.tsx
+++ src/components/Widget/WidgetContent.tsx
@@ -7,13 +7,11 @@ interface WidgetContentProps {
 
 export function WidgetContent({ title, children }: WidgetContentProps) {
   return (
-    <section>
-      <h2 className="bg-zinc-200 px-5 py-2 text-sm font-medium text-zinc-600 dark:bg-zinc-950 dark:text-zinc-300">
+    <>
+      <h2 className="px-4 py-2 text-sm font-medium text-zinc-600 dark:text-zinc-300">
         {title}
       </h2>
-      <div className="divide-y-8 divide-zinc-300 dark:divide-zinc-950">
-        {children}
-      </div>
-    </section>
+      <div className="flex flex-col gap-1">{children}</div>
+    </>
   );
 }
