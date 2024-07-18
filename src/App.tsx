30d25026ace5ae1e556709257b8613d541543322
kauatwn <kaualopescontato@gmail.com>
2024-07-17 21:51:57 -0300

    Ajuste no app

--- src/App.tsx
+++ src/App.tsx
@@ -23,41 +23,14 @@ function App() {
           <Notification.Root>
             <Notification.Icon icon={Binary} />
             <Notification.Content text="You received an invitation" />
-            <Notification.Actions>
-              <Notification.Action icon={X} />
-              <Notification.Action
-                icon={Check}
-                className="bg-green-500 hover:bg-green-600 dark:bg-green-500 dark:hover:bg-green-600"
-              />
-            </Notification.Actions>
           </Notification.Root>
         </Widget.Content>
         <Widget.Content title="Older">
           {/* Older notifications section */}
           <Notification.Root>
-            <Notification.Icon icon={Binary} />
             <Notification.Content text="You received an invitation" />
-            <Notification.Actions>
-              <Notification.Action icon={X} />
-              <Notification.Action
-                icon={Check}
-                className="bg-green-500 hover:bg-green-600 dark:bg-green-500 dark:hover:bg-green-600"
-              />
-            </Notification.Actions>
           </Notification.Root>
           <Notification.Root>
-            <Notification.Icon icon={Binary} />
-            <Notification.Content text="You received an invitation" />
-            <Notification.Actions>
-              <Notification.Action icon={X} />
-              <Notification.Action
-                icon={Check}
-                className="bg-green-500 hover:bg-green-600 dark:bg-green-500 dark:hover:bg-green-600"
-              />
-            </Notification.Actions>
-          </Notification.Root>
-          <Notification.Root>
-            <Notification.Icon icon={Binary} />
             <Notification.Content text="You received an invitation" />
             <Notification.Actions>
               <Notification.Action icon={X} />
