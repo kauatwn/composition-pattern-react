8cf4d506011c563e1ea4ef3342b5b3170b9e918c
kauatwn <kaualopescontato@gmail.com>
2024-07-18 18:23:49 -0300

    Incluindo WidgetGroup cpmponent

--- src/App.tsx
+++ src/App.tsx
@@ -4,45 +4,45 @@ import { Widget } from "./components/Widget";
 
 function App() {
   return (
-    <>
+    <div className="flex h-screen w-screen items-center justify-center px-2 dark:bg-zinc-700">
       <Widget.Root>
         <Widget.Header title="Notifications" label="Mark all as read" />
-        <Widget.Content title="Recent">
-          {/* Recent notifications section */}
-          <Notification.Root>
-            <Notification.Icon icon={Binary} />
-            <Notification.Content text="You received an invitation" />
-            <Notification.Actions>
-              <Notification.Action icon={X} />
-              <Notification.Action
-                icon={Check}
-                className="bg-green-500 hover:bg-green-600 dark:bg-green-500 dark:hover:bg-green-600"
-              />
-            </Notification.Actions>
-          </Notification.Root>
-          <Notification.Root>
-            <Notification.Icon icon={Binary} />
-            <Notification.Content text="You received an invitation" />
-          </Notification.Root>
-        </Widget.Content>
-        <Widget.Content title="Older">
-          {/* Older notifications section */}
-          <Notification.Root>
-            <Notification.Content text="You received an invitation" />
-          </Notification.Root>
-          <Notification.Root>
-            <Notification.Content text="You received an invitation" />
-            <Notification.Actions>
-              <Notification.Action icon={X} />
-              <Notification.Action
-                icon={Check}
-                className="bg-green-500 hover:bg-green-600 dark:bg-green-500 dark:hover:bg-green-600"
-              />
-            </Notification.Actions>
-          </Notification.Root>
-        </Widget.Content>
+        <Widget.Group>
+          <Widget.Content title="Recent">
+            <Notification.Root>
+              <Notification.Icon icon={Binary} />
+              <Notification.Content text="You received an invitation" />
+              <Notification.Actions>
+                <Notification.Action icon={X} />
+                <Notification.Action
+                  icon={Check}
+                  className="bg-green-500 hover:bg-green-600 dark:bg-green-500 dark:hover:bg-green-600"
+                />
+              </Notification.Actions>
+            </Notification.Root>
+            <Notification.Root>
+              <Notification.Icon icon={Binary} />
+              <Notification.Content text="You received an invitation" />
+            </Notification.Root>
+          </Widget.Content>
+          <Widget.Content title="Older">
+            <Notification.Root>
+              <Notification.Content text="You received an invitation" />
+            </Notification.Root>
+            <Notification.Root>
+              <Notification.Content text="You received an invitation" />
+              <Notification.Actions>
+                <Notification.Action icon={X} />
+                <Notification.Action
+                  icon={Check}
+                  className="bg-green-500 hover:bg-green-600 dark:bg-green-500 dark:hover:bg-green-600"
+                />
+              </Notification.Actions>
+            </Notification.Root>
+          </Widget.Content>
+        </Widget.Group>
       </Widget.Root>
-    </>
+    </div>
   );
 }
 
